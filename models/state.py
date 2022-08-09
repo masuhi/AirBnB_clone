4ebce8183c6078a3da64b8c1d592ff8b5b21b730
masuhi <99360684+masuhi@users.noreply.github.com>
2022-08-10 00:14:54 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""`State` class that inherits from `BaseModel`
+"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-        """ State class """
-            name = ""
+    """Inherits from BaseModel to create a state"""
+    name = ""
