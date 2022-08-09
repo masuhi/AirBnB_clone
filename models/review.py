ac1d850bdc0db667c2eba9c050ef322039180456
masuhi <99360684+masuhi@users.noreply.github.com>
2022-08-10 00:14:24 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+"""`Review` class that inherits from `BaseModel`
+"""
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-        """ Review classto store review information """
-            place_id = ""
-                user_id = ""
-                    text = ""
+    """Inherits from `BaseModel to create a Review
+
+    Attributes:
+        place_id (str): to hold the place.id
+        user_id (str): to hold the user.id
+        text (str): to hold the text
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
